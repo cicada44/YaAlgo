9512a5fdcea7b4f8eec186e7b9dd454b48bb67ae
Cicada <hubbleadd@bk.ru>
2023-05-01 20:42:24 +0700

    Contest 1: Add G colution

--- contest-01/F.cpp
+++ contest-01/F.cpp
@@ -7,18 +7,11 @@ int main() {
 
     std::cin >> a1 >> b1 >> a2 >> b2;
 
-    s1 = (a1 + a2) * ((b1 > b2) ? b1 : b2);
-    s2 = (a1 + b2) * ((a2 > b1) ? a2 : b1);
-    s3 = (b1 + a2) * ((b2 > a1) ? b2 : a1);
-    s4 = (b1 + b2) * ((a1 > a2) ? a1 : a2);
-
-    // std::cout << s1 << ' ' << s2 << ' ' << s3 << ' ' << s4 << ' ' << '\n';
-
     std::vector<std::pair<int, std::pair<int, int>>> total{
-        {s1, {a1 + a2, ((b1 > b2) ? b1 : b2)}},
-        {s2, {a1 + b2, ((a2 > b1) ? a2 : b1)}},
-        {s3, {b1 + a2, ((b2 > a1) ? b2 : a1)}},
-        {s4, {b1 + b2, ((a1 > a2) ? a1 : a2)}}};
+        {(a1 + a2) * ((b1 > b2) ? b1 : b2), {a1 + a2, ((b1 > b2) ? b1 : b2)}},
+        {(a1 + b2) * ((a2 > b1) ? a2 : b1), {a1 + b2, ((a2 > b1) ? a2 : b1)}},
+        {(b1 + a2) * ((b2 > a1) ? b2 : a1), {b1 + a2, ((b2 > a1) ? b2 : a1)}},
+        {(b1 + b2) * ((a1 > a2) ? a1 : a2), {b1 + b2, ((a1 > a2) ? a1 : a2)}}};
 
     auto min =
         std::min_element(total.begin(), total.end(),
---
+++ contest-01/G.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+    int N, K, M, total, aod, aob, lb, ld;
+
+    total = aod = aob = lb = ld = 0;
+
+    std::cin >> N >> K >> M;
+
+    while (N >= K && K >= M) {
+        aod = K / M;
+        aob = N / K;
+        ld = (K % M) * aob;
+        lb = N % K;
+
+        N = lb + ld;
+
+        total += aob * aod;
+
+        // std::cout << "\naod - " << aod << '\n'
+        //           << "aob - " << aob << '\n'
+        //           << "ld - " << ld << '\n'
+        //           << "lb - " << lb << '\n';
+    }
+
+    std::cout << total;
+}
