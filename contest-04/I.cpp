c6dd227e2cb1fbf7b339f76cf3ae23c49bd9d7b5
Cicada <hubbleadd@bk.ru>
2023-06-12 23:31:22 +0700

    Contest 4: Add I solution

--- contest-04/I.cpp
+++ contest-04/I.cpp
@@ -1,30 +1,39 @@
 #include <algorithm>
 #include <iostream>
-#include <map>
 #include <sstream>
 #include <string>
+#include <unordered_map>
 #include <unordered_set>
 
 int main() {
-    std::unordered_set<std::string> dict;
+    std::unordered_map<std::string, std::unordered_set<std::string>> dict;
     std::string inputableWord, sentence, word, naturalWord;
     int nMistakes = 0, nDict;
 
     std::cin >> nDict;
     for (int i = 0; i != nDict; ++i) {
-        std::cin >> inputableWord;
-        dict.insert(inputableWord);
+        std::cin >> word;
+        inputableWord = word;
+        std::for_each(inputableWord.begin(), inputableWord.end(),
+                      [](char& c) { c = std::tolower(c); });
+        dict.insert({inputableWord, {}});
+        dict[inputableWord].insert(word);
     }
     std::getline(std::cin >> std::ws, sentence, '\n');
     std::stringstream ssSentence(sentence);
 
     while (ssSentence >> word) {
-        if (dict.find(word) == dict.end()) {
-            int nUpper = 0;
-            for (const auto &c : word) {
-                if (std::isupper(c)) ++nUpper;
+        naturalWord = word;
+        std::for_each(naturalWord.begin(), naturalWord.end(), [](char& c) { c = std::tolower(c); });
+        if (dict.find(naturalWord) != dict.end()) {
+            if (dict[naturalWord].find(word) == dict[naturalWord].end() ||
+                std::count_if(word.begin(), word.end(),
+                              [](const char& c) { return std::isupper(c); }) != 1) {
+                ++nMistakes;
             }
-            if (nUpper != 1) { ++nMistakes; }
+        } else if (std::count_if(word.begin(), word.end(),
+                                 [](const char& c) { return std::isupper(c); }) != 1) {
+            ++nMistakes;
         }
     }
 
